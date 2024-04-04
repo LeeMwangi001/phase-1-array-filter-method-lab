adf3b002f31d32a7187eceed22728d3d1127d329
Lee <lee.mugweru@gmail.com>
2024-04-04 18:22:03 +0300

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(array,specifiedName) {
+    const driverName = array.filter(driver => {
+        return driver.toLowerCase() === specifiedName.toLowerCase() ; 
+    });
+    return driverName;
+}
+
+function fuzzyMatch(array, specifiedChar) {
+    const filteredArray = array.filter(driverChar => {
+        return driverChar.toLowerCase().indexOf(specifiedChar.toLowerCase()) === 0
+    });
+    return filteredArray;
+}
+function matchName(array, specifiedName){
+    return array.filter((array) => array.name === specifiedName);
+}
+
